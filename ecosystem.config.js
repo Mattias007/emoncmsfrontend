61d6ed4cc5cbca334de7b2930de616fb755507d3
Mattias007 <mattiasmaasik@ametikool.ee>
2024-03-12 17:51:19 +0200

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+      {
+        name: 'your-nextjs-app',
+        script: 'node_modules/next/dist/bin/next', // Path to Next.js binary
+        args: 'start', // Command-line arguments to pass to Next.js
+        instances: 'max', // Run as many instances as possible
+        autorestart: true, // Automatically restart the application if it crashes
+        watch: false, // Disable file watching to restart the application
+        max_memory_restart: '1G', // Maximum memory usage to automatically restart
+        env: {
+          NODE_ENV: 'production', // Set environment to production
+          PORT: 3000 // Set port for the application
+        }
+      }
+    ]
+  };
+  
